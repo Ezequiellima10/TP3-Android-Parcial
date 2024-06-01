416c436335650a3a60bf714f82cfd3105ba8aa41
Izak Warrior <ezequieldanielferreiralima@gmail.com>
2024-06-01 20:37:41 -0300

    diseño de los items y pantalla de home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.fragment.ktx)
+    implementation(libs.compose.theme.adapter)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
