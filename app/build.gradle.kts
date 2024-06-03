67e564220fee4f7a913fcc80124387a7e565436f
NicoTesta <nicolas_testa@outlook.com>
2024-06-03 18:34:42 -0300

    Cambios realizados: - Se implemento retrofit para que retorne los vuelos. - Se implemento un recyclerView para mostrar el detalle de cada uno de los vuelos.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,12 +45,11 @@ dependencies {
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.fragment.ktx)
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("com.squareup.picasso:picasso:2.71828")
+    implementation(libs.retrofit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
-
