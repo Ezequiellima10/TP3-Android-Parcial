a853822715e70e5f603346658ed578c8544e1a20
Izak Warrior <ezequieldanielferreiralima@gmail.com>
2024-06-04 15:08:54 -0300

    fix en los recycleviews de la Home

--- app/src/main/java/com/example/appandoid/providers/DestinationProvider.kt
+++ app/src/main/java/com/example/appandoid/providers/DestinationProvider.kt
@@ -1,5 +1,6 @@
 package com.example.appandoid.providers
 
+import com.example.appandoid.R
 import com.example.appandoid.models.homeModels.DestinationModel
 
 
@@ -9,19 +10,26 @@ class DestinationProvider {
                 DestinationModel(
                     "Boracay",
                     "Philippines",
-                    "5D4N"),
+                    "5D4N",
+                     R.drawable.boracay),
+
                 DestinationModel(
                     "Baros",
                     "Maldives",
-                    "7D6N"),
+                    "7D6N",
+                    R.drawable.baros),
+
                 DestinationModel(
                     "Bali",
                     "Indonesia",
-                    "3D2N"),
+                    "3D2N",
+                    R.drawable.bali),
+
                 DestinationModel(
                     "Palawan",
                     "Philippines",
-                    "3D2N"),
+                    "3D2N",
+                    R.drawable.palawan),
                 )
 
 
--- app/src/main/java/com/example/appandoid/ui/offers/OffersFragment.kt
+++ app/src/main/java/com/example/appandoid/ui/offers/OffersFragment.kt
@@ -9,14 +9,7 @@ import com.example.appandoid.R
 
 
 class OffersFragment : Fragment() {
-   // private lateinit var offerAdapter: OfferAdapter
-  /*  override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }*/
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,55 +18,6 @@ class OffersFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_offers, container, false)
 
-        /*val offersRecyclerView = view.findViewById<RecyclerView>(R.id.rec_card_offers)
-        offersRecyclerView.layoutManager =
-            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-        offerAdapter = OfferAdapter(OfferProvider.getOffersList(requireContext()))
-        offersRecyclerView.adapter = offerAdapter*/
-
-
-        //-------------------------------------------------------------------------------------
-        //val titleMatercard = getString(R.string.offers_rv_tiitulo_mastecard)
-
-        //val offersRecyclerView = view.findViewById<RecyclerView>(R.id.rec_card_offers)
-       // offersRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-
-
-      /*  val titleMatercard = "descuento master"
-        val titleVisa = "descuento visa"
-        val limitedOffer = "oferta limitada"
-        val descripcionVisa = "descrip"
-        val descripcionMaster= "descr"
-        val descripcionVisa2 = "descrip"
-        val descripcionMaster2= "descr"
-        val imageMastercard: Int = R.drawable.offer_mastercard
-        val imageVisa: Int = R.drawable.offer_visa
-
-
-        val ofertas = listOf(
-            OfferModel(titleMatercard,
-                descripcionMaster,
-                descripcionMaster2,
-                limitedOffer,
-                imageMastercard),
-            OfferModel(titleVisa,
-                descripcionVisa,
-                descripcionVisa2,
-                limitedOffer,
-                imageVisa)
-        )*/
-
-//----------------------------------------------------
-        // Initialize the adapter
-        //offerAdapter = OfferAdapter(ofertas) // Pass an empty list or your data here
-
-        // Set the adapter to the RecyclerView
-       // offersRecyclerView.adapter = offerAdapter
-
-
-
-
-//---------------------------------------------------
         return inflater.inflate(R.layout.fragment_offers, container, false)
     }
 
